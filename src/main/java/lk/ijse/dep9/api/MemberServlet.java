4ce1fcb7a9b47ace9baf74f996c8e8df9a342b17
Eranga Bandara <erangabandara94@gmail.com>
2022-11-13 11:20:10 +0530

    Implement loadAllMembers method

--- src/main/java/lk/ijse/dep9/api/MemberServlet.java
+++ src/main/java/lk/ijse/dep9/api/MemberServlet.java
@@ -1,13 +1,20 @@
 package lk.ijse.dep9.api;
 
 import jakarta.annotation.Resource;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.ijse.dep9.dto.MemberDTO;
 import lk.ijse.dep9.util.HttpServlet2;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 @WebServlet(name = "MemberServlet", value = "/members/*", loadOnStartup = 0)
 public class MemberServlet extends HttpServlet2 {
@@ -17,6 +24,30 @@ public class MemberServlet extends HttpServlet2 {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("members doGet()");
+    }
+
+    private void loadAllMembers(HttpServletResponse response) throws IOException {
+        try(Connection connection = pool.getConnection()) {
+            Statement stm = connection.createStatement();
+            String sql = "SELECT * FROM member";
+            ResultSet rst = stm.executeQuery(sql);
+            ArrayList<MemberDTO> members = new ArrayList<>();
+
+            while (rst.next()){
+                String id = rst.getString("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+                String contact = rst.getString("contact");
+                MemberDTO memberDTO = new MemberDTO(id, name, address, contact);
+                members.add(memberDTO);
+            }
+
+            response.setContentType("application/json");
+            JsonbBuilder.create().toJson(members, response.getWriter());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to fetch members");
+        }
     }
 
     @Override
