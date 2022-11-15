59c4c1ddbcdc1c0bf6576d71b3659c19370225de
Eranga Bandara <erangabandara94@gmail.com>
2022-11-15 11:24:12 +0530

    Validating issue-note URL

--- src/main/java/lk/ijse/dep9/api/IssueNoteServlet.java
+++ src/main/java/lk/ijse/dep9/api/IssueNoteServlet.java
@@ -1,16 +1,29 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
-@WebServlet(name = "IssueNoteServlet", value = "/issue-note")
+@WebServlet(name = "IssueNoteServlet", value = "/issue-notes")
 public class IssueNoteServlet extends HttpServlet {
 
+    @Resource(lookup = "java:comp/env/jdbc/lms")
+    private DataSource pool;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("issue-note doPost()");
+        if(request.getPathInfo() != null && !request.getPathInfo().equals("/")){
+            response.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED);
+            return;
+        }
+
+        if(request.getContentType() == null || !request.getContentType().startsWith("application/json")){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+            return;
+        }
+
     }
 }
